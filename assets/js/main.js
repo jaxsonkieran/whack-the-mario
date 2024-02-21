2264e7f294756eb721fbb205bbc04d099312632b
Kieran Jackson <jaxsonkieran@gmail.com>
2024-02-21 17:52:57 +0000

    add random time function, test in console

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,13 @@
 const holes = document.querySelectorAll('.hole');
 const marios = document.querySelectorAll('.mario');
 const score = document.querySelector('.score');
+
+
+/**
+ * function that will give a random amount of time between pop ups
+ */
+
+function randomTime(min, max){
+    return Math.random() * (max - min) + min;
+    
+}
